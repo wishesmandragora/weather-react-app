92228b229aee736e13fcbaa4e3f7ce321f1f2cdd
wishesmandragora <anabudimir98@gmail.com>
2023-05-07 05:55:17 +0200

    Added styles from original

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <input
+          type="text"
+          autoComplete="off"
+          className="search-box"
+          placeholder="Search for a city..."
+        />
       </header>
+
+      <main>
+        <section className="location">
+          <div className="city">
+            <h1 aria-hidden="true"></h1>
+            <div className="date"></div>
+          </div>
+        </section>
+        <div className="current">
+          <div className="temp">
+            <span className="num"></span>
+            <span></span>
+          </div>
+          <div className="weather"></div>
+          <div className="hi-low"></div>
+        </div>
+      </main>
     </div>
   );
 }
